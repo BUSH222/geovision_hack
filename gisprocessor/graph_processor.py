1b00639a42f9910bd12adf9cb6416e2b5f4eb652
Ted <58144503+BUSH222@users.noreply.github.com>
2024-05-24 18:48:12 +0300

    colors separated

--- gisprocessor/graph_processor.py
+++ gisprocessor/graph_processor.py
@@ -4,35 +4,50 @@ import numpy as np
 from PIL import Image
 
 
-def process_image(image, color_range):
-    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+def process_image(image, color_range, use_hsv=False):
+    if not use_hsv:
+        image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+        (R1, G1, B1), (R2, G2, B2) = color_range
+        lower_range = np.array([B1, G1, R1], dtype=np.uint8)
+        upper_range = np.array([B2, G2, R2], dtype=np.uint8)
+        mask = cv2.inRange(image, lower_range, upper_range)
+        mask_inv = cv2.bitwise_not(mask)
 
-    (R1, G1, B1), (R2, G2, B2) = color_range
-    lower_range = np.array([B1, G1, R1], dtype=np.uint8)
-    upper_range = np.array([B2, G2, R2], dtype=np.uint8)
+        image[mask_inv > 0] = [255, 255, 200]
 
-    mask = cv2.inRange(image, lower_range, upper_range)
-    mask_inv = cv2.bitwise_not(mask)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    image[mask_inv > 0] = [255, 255, 200]
+        # _, binary = cv2.threshold(gray, 0, 0, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        large_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > 50]
+        mask = np.zeros_like(gray)
+        cv2.drawContours(mask, large_contours, -1, (255), thickness=cv2.FILLED)
 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cleaned_image = cv2.bitwise_and(image, image, mask=mask)
 
-    # _, binary = cv2.threshold(gray, 0, 0, cv2.THRESH_BINARY)
-    contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    large_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > 50]
-    mask = np.zeros_like(gray)
-    cv2.drawContours(mask, large_contours, -1, (255), thickness=cv2.FILLED)
+        cleaned_image = Image.fromarray(cv2.cvtColor(cleaned_image, cv2.COLOR_BGR2RGB))
 
-    cleaned_image = cv2.bitwise_and(image, image, mask=mask)
+        return cleaned_image
+    else:
+        image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2HSV)
+        (R1, G1, B1), (R2, G2, B2) = color_range
+        lower_range = np.array([R1, G1, B1])
+        upper_range = np.array([R2, G2, B2])
+        lower_range2 = np.array([180-R2, G1, B1])
+        upper_range2 = np.array([180-R1, G2, B2])
+        mask = cv2.inRange(image, lower_range, upper_range)
+        mask2 = cv2.inRange(image, lower_range2, upper_range2)
+        image = cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
+        mask_inv1 = cv2.bitwise_not(mask)
+        mask_inv2 = cv2.bitwise_not(mask2)
+        mask_inv = cv2.bitwise_and(mask_inv1, mask_inv2)
+        image[mask_inv > 0] = [200, 255, 255]
+        return Image.fromarray(image)
 
-    cleaned_image = Image.fromarray(cv2.cvtColor(cleaned_image, cv2.COLOR_BGR2RGB))
 
-    return cleaned_image
+if __name__ == "__main__":
+    prepro = process_image(pil_convert('DATA/well_3_old.jpg'), [[0, 70, 70], [40, 255, 255]], use_hsv=True)
+    prepro.save('DATA/OUT/res0.png')
 
-
-prepro = process_image(pil_convert('DATA/well_3_old.jpg'), [[130, 70, 70], [200, 160, 160]])
-prepro.save('DATA/OUT/res0.png')
-
-prepro2 = process_image(pil_convert('DATA/well_3_old.jpg'), [[0, 0, 0], [100, 100, 100]])
-prepro2.save('DATA/OUT/res1.png')
+    # prepro2 = process_image(pil_convert('DATA/well_3_old.jpg'), [[0, 0, 0], [100, 100, 100]])
+    # prepro2.save('DATA/OUT/res1.png')
