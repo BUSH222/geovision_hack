01f109bb82ed971ad5b234fb642ac48eb4d16cfe
ShagDasha10 <shagaeva2005@gmail.com>
2024-05-25 00:29:17 +0300

    task3

--- web_interface/static/script.js
+++ web_interface/static/script.js
@@ -1,37 +1,47 @@
-let selectedFilesInfo = []; // Глобальная переменная для хранения информации о файлах
+let selectedFilesInfo = [];
 
-        function saveFileInfo(e) {
-            const files = e.target.files;
-            if (files.length > 0) {
-                Array.from(files).forEach((file, index) => {
-                    selectedFilesInfo.push({
-                        name: file.name,
-                        type: file.type,
-                        size: file.size,
-                        lastModifiedDate: file.lastModifiedDate
-                    });
-                });
-            }
-        }
+function saveFileInfo(e) {
+    const files = e.target.files;
+    if (files.length > 0) {
+        Array.from(files).forEach((file, index) => {
+            selectedFilesInfo.push({
+                name: file.name,
+                type: file.type,
+                size: file.size,
+                lastModifiedDate: file.lastModifiedDate,
+                blob: file.slice(0, file.size) // Получаем Blob объект файла
+            });
+        });
+    }
+}
 
 document.getElementById('uploadForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Предотвращаем стандартное действие формы
+    event.preventDefault();
 
-    var formData = new FormData();
-    Array.from(this.elements.namedItem('file').files).forEach((file, index) => {
-        formData.append(`file${index}`, file);
+    // Преобразование каждого Blob в строку Base64
+    const base64Files = selectedFilesInfo.map(file => {
+        return btoa(
+            new Uint8Array(file.blob).reduce(
+                (data, byte) => data + String.fromCharCode(byte),
+                ''
+            )
+        );
     });
 
-    // Добавляем информацию о файлах в FormData
-    selectedFilesInfo.forEach((info, index) => {
-        formData.append(`fileInfo${index}`, info.name); // Здесь предполагается, что сервер ожидает имя файла
-    });
-    // Отправляем данные на сервер в формате JSON
-    res = fetch('/', {
+    // Создание объекта JSON с данными файлов
+    const jsonData = {
+        filesInfo: base64Files
+    };
+
+    // Отправка данных на сервер в формате JSON
+    fetch('/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ filesInfo: selectedFilesInfo }) // Передача всей информации о файлах в виде JSON
-    });
+        body: JSON.stringify(jsonData)
+    })
+   .then(response => response.json())
+   .then(data => console.log(data))
+   .catch(error => console.error(error));
 });
