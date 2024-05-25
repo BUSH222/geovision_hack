69d5b4b441bbfeb7da706cdb1c70f52b4d3119c5
nexxy06 <nikpan254@gmail.com>
2024-05-25 17:19:47 +0300

    json

---
+++ gisprocessor/ImportToJson.py
@@ -0,0 +1,9 @@
+import json
+
+
+def read_json_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    lst = []
+    for key, value in data.items():
+        lst.append((key, value))
